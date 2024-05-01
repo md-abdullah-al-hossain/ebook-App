48342def358633242eaa7b5e0914073fe8b9681a
DURJOY <mrd9676@gmail.com>
2024-05-01 22:03:29 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:ebook_app/source/common/color_extenstion.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        primaryColor: TColor.primary,
+        fontFamily: 'SF Pro Text',
+      ),
+
+      // home: const HomeScreen(),
+    );
+  }
+}
