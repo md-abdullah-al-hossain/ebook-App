90c838c04d80653ec97ab30cb030dae10857fbe1
DURJOY <mrd9676@gmail.com>
2024-05-03 10:45:21 +0600

    Create sign in Screen

---
+++ lib/src/common/widgets/outline_buttobn.dart
@@ -0,0 +1,35 @@
+import 'package:ebook_app/src/common/theme/app_color/color_extenstion.dart';
+import 'package:flutter/material.dart';
+
+class CustomOutlineButton extends StatelessWidget {
+  final String text;
+  final Color textColor;
+  final double borderWidth;
+
+  const CustomOutlineButton({
+    super.key,
+    required this.text,
+    required this.textColor,
+    this.borderWidth = 2,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: TColor.primary, width: borderWidth),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 20,
+            color: textColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
