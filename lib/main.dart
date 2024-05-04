8889dff2b6799613bd9f34fdd6735649a9f0e202
DURJOY <mrd9676@gmail.com>
2024-05-04 11:58:10 +0600

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:ebook_app/src/themes/app_color/color_extenstion.dart';
-import 'package:ebook_app/src/screens/authentication/forgot_password.dart';
-import 'package:ebook_app/src/screens/authentication/sign_up.dart';
-import 'package:ebook_app/src/screens/etc/help_us.dart';
+
+import 'package:ebook_app/common/color_extenstion.dart';
+import 'package:ebook_app/view/main_tab/main_tab_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,15 +14,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primaryColor: TColor.primary,
+      
         fontFamily: 'SF Pro Text',
       ),
-      home: HelpUs(KeyboardType: null,),
+      home:  MainTabView() ,
     );
   }
 }
+
